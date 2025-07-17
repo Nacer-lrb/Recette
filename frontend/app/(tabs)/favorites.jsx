6229f7f0f686fffdad05d9b5a7ce871d7d8c6c02
Nacer-lrb <laribinacer2002@gmail.com>
2025-07-17 11:00:07 +0100

    loadFavorites

--- frontend/app/(tabs)/favorites.jsx
+++ frontend/app/(tabs)/favorites.jsx
@@ -1,7 +1,43 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import {useClerk,useUser} from "@clerk/clerk-expo"
+import { useEffect,useState } from 'react'
+
+
 
 const FavoritesScreen = () => {
+  const {signOut} =useClerk()
+  const {user}= useUser()
+  const {favotiteRecipes,setFavoriteRevipes} = useState()
+  const {loading,setLoading}=useEffect()
+
+  useEffect(()=>{
+    const loadFavorites = async() =>{
+      try{
+  const responce = await fetch(`${API_URL}/api/favorites/${user.id}`)
+  if(!responce.ok) throw new Error("Failed to fetch favorites")
+
+    const favorites = await responce.json();
+
+      // transform the data to match the RecipeCard component's expected format
+      const transformedFavorites = favorites.map((favorite) => ({
+        ...favorite,
+        id: favorite.recipeId,
+      }));
+
+    setFavoriteRevipes(transformedFavorites)
+
+      } catch (error) {
+        console.log("Error loading favorites", error);
+        Alert.alert("Error", "Failed to load favorites");
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadFavorites();
+
+  },[user.id]);
+
+
   return (
     <View>
       <Text>FavoritesScreen</Text>
---
+++ frontend/constants/api.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://recette-1.onrender.com"
