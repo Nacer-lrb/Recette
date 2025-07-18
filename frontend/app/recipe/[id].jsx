fe5dbdab0e0043fb809aca46ec721973d087f4b9
Nacer-lrb <laribinacer2002@gmail.com>
2025-07-18 11:03:31 +0100

    save and delete favorites

--- frontend/app/recipe/[id].jsx
+++ frontend/app/recipe/[id].jsx
@@ -62,6 +62,55 @@ import {MealAPI} from "../../services/mealAPI"
 
     },[recipeId, userId]);
    
+    const getYouTubeEmbedUrl = (url) => {
+      // example url: https://www.youtube.com/watch?v=mTvlmY4vCug
+      const videoId = url.split("v=")[1];
+      return `https://www.youtube.com/embed/${videoId}`;
+    };
+
+    const handleToggleSave = async () => {
+      setIsSaving(true);
+  
+      try {
+        if (isSaved) {
+          // remove from favorites
+          const response = await fetch(`${API_URL}/api/favorites/${userId}/${recipeId}`, {
+            method: "DELETE",
+          });
+          if (!response.ok) throw new Error("Failed to remove recipe");
+  
+          setIsSaved(false);
+        } else {
+          // add to favorites
+          const response = await fetch(`${API_URL}/api/favorites`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              userId,
+              recipeId: parseInt(recipeId),
+              title: recipe.title,
+              image: recipe.image,
+              cookTime: recipe.cookTime,
+              servings: recipe.servings,
+            }),
+          });
+  
+          if (!response.ok) throw new Error("Failed to save recipe");
+          setIsSaved(true);
+        }
+      } catch (error) {
+        console.error("Error toggling recipe save:", error);
+        Alert.alert("Error", `Something went wrong. Please try again.`);
+
+        
+      } finally {
+        setIsSaving(false);
+      }
+    };
+  
+
 
 
 
